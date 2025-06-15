95ce97f592d155c8f23f3d4050480389e36ad552
Tamas <tamas0225@gmail.com>
2025-06-15 21:08:50 +0200

    extend functionalities

--- note/urls.py
+++ note/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from notes.views import NoteViewSet
+from django.contrib import admin
+from django.urls import path
 
 router = DefaultRouter()
 router.register(r'notes', NoteViewSet)
 
 urlpatterns = [
     path('api/', include(router.urls)),
+    path('admin/', admin.site.urls),  # Ez kell az admin felülethez
 ]
--- notes/admin.py
+++ notes/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from .models import Note  # Importáljuk a Note modellt
 
-# Register your models here.
+admin.site.register(Note)  # Regisztráljuk az admin felületen
